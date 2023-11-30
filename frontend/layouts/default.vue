3520c47c34d5aba79a505aeda0c3fd9419bfd601
harvdt <harviandito20@gmail.com>
2023-11-30 21:52:20 +0700

    add frontend

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+    <nav class="bg-white border-gray-200 dark:bg-gray-900">
+      <div class="max-w-screen-xl flex items-center justify-between mx-auto p-4">
+        <h1 class="text-2xl font-semibold text-gray-900 dark:text-white">IT BLOG</h1>
+        <ul class="flex space-x-6">
+          <li><NuxtLink to="/" class="text-gray-700 hover:text-gray-900 dark:text-white dark:hover:text-gray-300">Home</NuxtLink></li>
+          <li><NuxtLink to="/about" class="text-gray-700 hover:text-gray-900 dark:text-white dark:hover:text-gray-300">About</NuxtLink></li>
+          <li><NuxtLink to="/article" class="text-gray-700 hover:text-gray-900 dark:text-white dark:hover:text-gray-300">Article</NuxtLink></li>
+        </ul>
+      </div>
+    </nav>
+  </template>
---
+++ frontend/pages/about.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="max-w-screen-md mx-auto py-8 px-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div v-for="(person, index) in people" :key="index" class="bg-white rounded-lg shadow-md overflow-hidden">
+        <img :src="person.image" alt="Person Image" class="w-full h-48 object-cover object-center" />
+        <div class="p-4">
+          <h2 class="text-xl font-semibold mb-2">{{ person.name }}</h2>
+          <p>{{ person.motto }}</p>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        people: [
+          {
+            name: 'Muhammad Harvian Dito Syahputra',
+            image: 'https://via.placeholder.com/300',
+            motto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+          },
+          {
+            name: 'Ilhan Ahmad Syafa',
+            image: 'https://via.placeholder.com/300',
+            motto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+          },
+          {
+            name: 'Gavriel Kurniaadi',
+            image: 'https://via.placeholder.com/300',
+            motto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+          }
+        ]
+      };
+    }
+  };
+  </script>
