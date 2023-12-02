2f139ee05f4cf52b3cf9db6c3db919d30143b567
Ilhan Ahmad <ilhanahmadsyf@gmail.com>
2023-12-02 18:46:50 +0700

    feat: ssg on home and about page

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="bg-white border-gray-200 dark:bg-gray-900">
       <div class="max-w-screen-xl flex items-center justify-between mx-auto p-4">
-        <h1 class="text-2xl font-semibold text-gray-900 dark:text-white">IT BLOG</h1>
+        <h1 class="text-2xl font-semibold text-gray-900 dark:text-white">1News</h1>
         <ul class="flex space-x-6">
           <li><NuxtLink to="/" class="text-gray-700 hover:text-gray-900 dark:text-white dark:hover:text-gray-300">Home</NuxtLink></li>
           <li><NuxtLink to="/about" class="text-gray-700 hover:text-gray-900 dark:text-white dark:hover:text-gray-300">About</NuxtLink></li>
